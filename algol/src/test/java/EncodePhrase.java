73df5288f6f4fd83b63a8898823893a1fa8d0993
Dima Hrynevych <dima.hrynevych@godaddy.com>
2018-02-02 16:16:16 +0200

    Encode phrase test

--- algol/src/test/java/EncodePhrase.java
+++ algol/src/test/java/EncodePhrase.java
@@ -1,10 +1,6 @@
 import org.testng.annotations.Test;
 
 public class EncodePhrase {
-    /*
-
-     */
-
 
     @Test
     public void EncodeTest() {
@@ -12,15 +8,16 @@ public class EncodePhrase {
         String encPhrase;
         int salt = 0x3;
 
-        encPhrase = new String(  encodePhrase(phrase, salt));
-         System.out.println(encPhrase);
+        encPhrase = new String(encodePhrase(phrase, salt, true));
+        System.out.println(encPhrase);
+        String decPhrase =  new String(encodePhrase(encPhrase, salt, false));
+        System.out.println(decPhrase);
     }
 
-    private char[] encodePhrase(String phrase, int salt) {
+    private char[] encodePhrase(String phrase, int salt, boolean encode) {
         char[] chPhrase;
         char[] encChPhrase;
-        char encCh ='*';
-        int ascii;
+        char encCh = '*';
         int counter = 0;
         chPhrase = phrase.toCharArray();
         encChPhrase = new char[chPhrase.length];
@@ -30,13 +27,18 @@ public class EncodePhrase {
                 encCh = ch;
             if ('0' <= ch && ch <= '9') {
                 int i = Character.getNumericValue(ch);
-                encCh = Character.forDigit((i + salt)%10, 10);
+                if (encode)
+                    encCh = Character.forDigit((i + salt) % 10, 10);
+                if (!encode)
+                    encCh = Character.forDigit((i - salt + 10) % 10, 10);
             }
             if (('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z')) {
                 int i = (int) ch;
-                encCh = (char) (i + salt);
+                if (encode)
+                    encCh = (char) (i + salt);
+                if (!encode)
+                    encCh = (char) (i - salt);
             }
-
             encChPhrase[counter] = encCh;
             counter++;
         }
